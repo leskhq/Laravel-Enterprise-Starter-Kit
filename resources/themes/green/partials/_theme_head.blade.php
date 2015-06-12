c44b9df609897fc64dfb52719b11fe2461ffb157
Sebastien Routier <sroutier@gmail.com>
2015-06-12 18:04:36 -0400

    Adding 2 themes, red and green.

---
+++ resources/themes/green/partials/_theme_head.blade.php
@@ -0,0 +1,5 @@
+<!-- AdminLTE Skins. We have chosen the skin-green for this starter
+                                                      page. However, you can choose any other skin. Make sure you
+      apply the skin class to the body tag so the changes take effect.
+-->
+<link href="{{ asset("/bower_components/admin-lte/dist/css/skins/skin-green.min.css") }}" rel="stylesheet" type="text/css" />
---
+++ resources/themes/red/partials/_body.blade.php
@@ -0,0 +1,23 @@
+<!--
+BODY TAG OPTIONS:
+=================
+Apply one or more of the following classes to get the
+desired effect
+|---------------------------------------------------------|
+| SKINS         | skin-blue                               |
+|               | skin-black                              |
+|               | skin-purple                             |
+|               | skin-yellow                             |
+|               | skin-red                                |
+|               | skin-green                              |
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | fixed                                   |
+|               | layout-boxed                            |
+|               | layout-top-nav                          |
+|               | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+-->
+<body class="skin-red sidebar-mini">
+    @include('partials._body_content')
+</body>
