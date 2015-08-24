ce6afa53149003ee0193437ad004cdc46c691fef
Sebastien Routier <sroutier@gmail.com>
2015-08-24 16:29:15 -0400

    Added master reference to CSRF token for all AJAX call to use.

--- resources/themes/default/layouts/master.blade.php
+++ resources/themes/default/layouts/master.blade.php
@@ -9,6 +9,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <title>L51ESK | {{ $page_title or "Page Title" }}</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Set a meta reference to the CSRF token for use in AJAX request -->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <!-- Bootstrap 3.3.4 -->
     <link href="{{ asset("/bower_components/admin-lte/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
     <!-- Font Awesome Icons 4.4.0 -->
