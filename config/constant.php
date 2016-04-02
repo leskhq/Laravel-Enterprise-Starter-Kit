598b485bb5d14b9e372fd978d443a1c5d7c0fc02
aquino <aquinotgr00@gmail.com>
2016-04-02 16:00:22 +0700

    define global variables
    defining some my global variables

--- config/constant.php
+++ config/constant.php
@@ -1,6 +1,33 @@
 <?php
 return [
 
-  
+  'tipe-customer' => [
+    '1' => 'Mitra BO Lisensi',
+    '2' => 'Agen Resmi',
+    '3' => 'Agen Lepas',
+    '4' => 'Customer Biasa',
+    '5' => 'Distributor',
+    '6' => 'Mitra BO Murni',
+    '7' => 'Investor'
+  ],
+
+  'banks' => [
+    ""         => "choose",
+    "BCA"      => "BCA",
+    "Mandiri"  => "Mandiri",
+    "BNI"      => "BNI",
+    "Muamalat" => "Muamalat"
+  ],
+
+  'product-category' => [
+    '1' => 'Detergent & Pewangi',
+    '2' => 'Anti Noda & Spotter',
+    '3' => 'Produk Rumah Tangga',
+    '4' => 'Produk Otomotif',
+    '5' => 'Perlengkapan',
+    '6' => 'Bibit Parfum',
+    '7' => 'Bahan Baku',
+    '8' => 'Parfum'
+  ],
 
 ];
