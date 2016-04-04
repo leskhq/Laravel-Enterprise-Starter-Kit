0af3b454c45f0af9bb9a394aebfb0253de68d272
aquino <aquinotgr00@gmail.com>
2016-04-04 15:21:13 +0700

    favicon
    adding favicon and change the web global title

--- resources/themes/default/layouts/master.blade.php
+++ resources/themes/default/layouts/master.blade.php
@@ -6,7 +6,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>L51ESK | {{ $page_title or "Page Title" }}</title>
+    <title>LC Web-App | {{ $page_title or "Page Title" }}</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Set a meta reference to the CSRF token for use in AJAX request -->
@@ -19,6 +19,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link href="{{ asset("/bower_components/admin-lte/ionicons/css/ionicons.min.css") }}" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
     <link href="{{ asset("/bower_components/admin-lte/dist/css/AdminLTE.min.css") }}" rel="stylesheet" type="text/css" />
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}">
 
     <!-- Application CSS-->
     <link href="{{ asset(elixir('css/all.css')) }}" rel="stylesheet" type="text/css" />
