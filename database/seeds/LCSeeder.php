644b36ea2a3f7fad3a747945eff1f086c911c181
aquino <aquinotgr00@gmail.com>
2016-04-18 13:52:49 +0700

    add permission and roles related to admin auth

--- database/seeds/LCSeeder.php
+++ database/seeds/LCSeeder.php
@@ -24,6 +24,14 @@ class LCSeeder extends Seeder
 
         ////////////////////////////////////
         // Create custom set of permissions
+        $permBasicAdminAuthenticated = Permission::create([
+            'name'         => 'basic-admin-authenticated',
+            'display_name' => 'Basic Admin Authenticated',
+            'description'  => 'Allows a user to view the site index of each features.',
+            'enabled'      => true,
+        ]);
+
+        // TODO: create manage employee permission
         $permManageExpeditions = Permission::create([
             'name'         => 'manage-expeditions',
             'display_name' => 'Manage expeditions',
@@ -48,6 +56,12 @@ class LCSeeder extends Seeder
             'description'  => 'Allows a user to manage the site customers and the simmilar features.',
             'enabled'      => true,
         ]);
+        $permManageFollowups = Permission::create([
+            'name'         => 'manage-followups',
+            'display_name' => 'Manage Followups',
+            'description'  => 'Allows a user to manage the site followups and the simmilar features.',
+            'enabled'      => true,
+        ])
         $permManageSales = Permission::create([
             'name'         => 'manage-sales',
             'display_name' => 'Manage sales',
@@ -60,76 +74,96 @@ class LCSeeder extends Seeder
             'display_name' => 'Update Sale Status',
             'description'  => 'Allows a user to update the sale status.',
             'enabled'      => true,
-            ]);
+        ]);
 
         ////////////////////////////////////
         // Associate permission to routes
+        ////////////////////////////////////
+        //  expedition routes
         $routeExpeditionIndex = Route::where('name', 'admin.expeditions.index')->first();
+        $routeExpeditionIndex->permission()->associate($permBasicAdminAuthenticated);
+        $routeExpeditionIndex->save();
         // get all the expedition routes
         $routeExpeditions     = Route::where('name', 'like', 'admin.expeditions.'.'%')->get();
         // assign the manage expeditions perms to each expedition routes
         foreach ($routeExpeditions as $key => $value) {
-            $value->permission()->associate($permManageExpeditions);
-            $value->save();
+            if ($value->name != 'admin.expeditions.index') {
+                $value->permission()->associate($permManageExpeditions);
+                $value->save();
+            }
         }
-
+        //  product routes
         $routeProductIndex = Route::where('name', 'admin.products.index')->first();
+        $routeProductIndex->permission()->associate($permBasicAdminAuthenticated);
+        $routeProductIndex->save();
         // get all the product routes
         $routeProducts     = Route::where('name', 'like', 'admin.products.'.'%')->get();
         // assign the manage products perms to each product routes
         foreach ($routeProducts as $key => $value) {
-            $value->permission()->associate($permManageProducts);
-            $value->save();
+            if ($value->name != 'admin.products.index') {
+                $value->permission()->associate($permManageProducts);
+                $value->save();
+            }
         }
-
+        //  supplier routes
         $routeSupplierIndex = Route::where('name', 'admin.suppliers.index')->first();
+        $routeSupplierIndex->permission()->associate($permBasicAdminAuthenticated);
+        $routeSupplierIndex->save();
         // get all the Supplier routes
         $routeSuppliers     = Route::where('name', 'like', 'admin.suppliers.'.'%')->get();
         // assign the manage suppliers perms to each supplier routes
         foreach ($routeSuppliers as $key => $value) {
-            $value->permission()->associate($permManageSuppliers);
-            $value->save();
+            if ($value->name != 'admin.suppliers.index') {
+                $value->permission()->associate($permManageSuppliers);
+                $value->save();
+            }
         }
-
+        //  customer routes
+        $routeCustomerIndex = Route::where('name', 'admin.customers.index')->first();
+        $routeCustomerIndex->permission()->associate($permBasicAdminAuthenticated);
+        $routeCustomerIndex->save();
         // get all the Customer routes
         $routeCustomers = Route::where('name', 'like', 'admin.customers.'.'%')->get();
         // assign the manage customers perms to each customer routes
         foreach ($routeCustomers as $key => $value) {
-            $value->permission()->associate($permManageCustomers);
-            $value->save();
+            if ($value->name != 'admin.customers.index') {
+                $value->permission()->associate($permManageCustomers);
+                $value->save();
+            }
         }
-
+        //  customer candidate routes
         $routeCustomerCandidateIndex  = Route::where('name', 'admin.customer-candidates.index')->first();
-        $routeCustomerCandidateCreate = Route::where('name', 'admin.customer-candidates.create')->first();
+        $routeCustomerCandidateIndex->permission()->associate($permBasicAdminAuthenticated);
+        $routeCustomerCandidateIndex->save();
         // get all the customer candidate routes
         $routeCustomerCandidates = Route::where('name', 'like', 'admin.customer-candidates.'.'%')->get();
         // assign the manage customers perms to each customer candidate routes
         foreach ($routeCustomerCandidates as $key => $value) {
-            $value->permission()->associate($permManageCustomers);
-            $value->save();
+            if ($value->name != 'admin.customer-candidates.index') {
+                $value->permission()->associate($permManageCustomers);
+                $value->save();
+            }
         }
-
-        $routeCustomerFollowupIndex = Route::where('name', 'admin.customer-followups.index')->first();
+        //  customer followup routes
         // get all the customer followup routes
         $routeCustomerFollowups = Route::where('name', 'like', 'admin.customer-followups.'.'%')->get();
         // assign the manage customers perms to each customer followup routes
         foreach ($routeCustomerFollowups as $key => $value) {
-            $value->permission()->associate($permManageCustomers);
+            $value->permission()->associate($permManageFollowups);
             $value->save();
         }
-
-        $routeCandidateFollowupIndex = Route::where('name', 'admin.candidate-followups.index')->first();
+        //  customer candidate followup routes
         // get all the customer candidate followup routes
         $routeCandidateFollowups = Route::where('name', 'like', 'admin.candidate-followups.'.'%')->get();
         // assign the manage candidate followup perms to each customer candidate followup routes
         foreach ($routeCandidateFollowups as $key => $value) {
-            $value->permission()->associate($permManageCustomers);
+            $value->permission()->associate($permManageFollowups);
             $value->save();
         }
-
-        $routeSalesUpdateStatus = Route::where('name', 'admin.sales.update-status')->first();
-        $routeSalesUpdateStatus->permission()->associate($permUpdateStatus);
-        $routeSalesUpdateStatus->save();
+        //  sale routes
+        $routeSaleUpdateStatus = Route::where('name', 'admin.sales.update-status')->first();
+        $routeSaleUpdateStatus->permission()->associate($permUpdateStatus);
+        $routeSaleUpdateStatus->save();
         // get all the sale routes
         $routeSales = Route::where('name', 'like', 'admin.sales.'.'%')->get();
         // assign the manage sale perms to each sale routes
@@ -148,6 +182,7 @@ class LCSeeder extends Seeder
             "description"   => "Secretaries are granted all permissions to the Admin|Sales section",
             "enabled"       => true
             ]);
+        $roleSecretaries->perms()->attach($permBasicAdminAuthenticated->id);
         $roleSecretaries->perms()->attach($permManageSales->id);
         $roleSecretaries->perms()->attach($permUpdateStatus->id);
 
@@ -157,6 +192,7 @@ class LCSeeder extends Seeder
             "description"   => "Marketings are granted all permissions to the Admin|Customers, Followup section",
             "enabled"       => true
             ]);
+        $roleMarketings->perms()->attach($permBasicAdminAuthenticated->id);
         $roleMarketings->perms()->attach($permManageCustomers->id);
 
         $rolePurcashings = Role::create([
@@ -165,6 +201,7 @@ class LCSeeder extends Seeder
             "description"   => "Purcashings are granted all permissions to the Admin|Suppliers, Expeditions section",
             "enabled"       => true
             ]);
+        $rolePurcashings->perms()-.attach($permBasicAdminAuthenticated->id);
         $rolePurcashings->perms()->attach($permManageSuppliers->id);
         $rolePurcashings->perms()->attach($permManageExpeditions->id);
 
@@ -174,9 +211,10 @@ class LCSeeder extends Seeder
             "description"   => "Productions are granted all permissions to the Admin|Products and some Sales section",
             "enabled"       => true
             ]);
+        $roleProductions->perms()->attach($permBasicAdminAuthenticated->id);
         $roleProductions->perms()->attach($permManageProducts->id);
         $roleProductions->perms()->attach($permUpdateStatus->id);
-        
+
         $roleOperationals = Role::create([
             "name"          => "operationals",
             "display_name"  => "Operationals",
@@ -192,6 +230,7 @@ class LCSeeder extends Seeder
             "description"   => "HRGA are granted all permissions to the Admin|Employes section",
             "enabled"       => true
             ]);
+        $roleHrga->perms()->attach($permBasicAdminAuthenticated->id);
         // TODO : add permission to manage the empoloyes section
 
         ////////////////////////////////////
@@ -258,7 +297,7 @@ class LCSeeder extends Seeder
             'url'           => null,
             'enabled'       => true,
             'parent_id'     => $menuCustomersParent->id,            // Parent is customers
-            'route_id'      => $routeCustomerCandidateCreate->id,   // Route to customer candidate create
+            'route_id'      => Route::where('name', 'admin.customer-candidates.create')->first()->id,   // Route to customer candidate create
             'permission_id' => null,                                // Get permission from route
         ]);
 
@@ -346,7 +385,7 @@ class LCSeeder extends Seeder
             'url'           => null,
             'enabled'       => true,
             'parent_id'     => $menuCustomersParent->id,            // Parent is customers
-            'route_id'      => $routeCustomerFollowupIndex->id,     // Route to customer followup index
+            'route_id'      => Route::where('name', 'admin.customer-followups.index')->first()->id,     // Route to customer followup index
             'permission_id' => null,                                // Get permission from route
         ]);
         $menuCandidateFollowups = Menu::create([
@@ -358,7 +397,7 @@ class LCSeeder extends Seeder
             'url'           => null,
             'enabled'       => true,
             'parent_id'     => $menuCustomersParent->id,            // Parent is customers
-            'route_id'      => $routeCandidateFollowupIndex->id,    // Route to customer candidate followup index
+            'route_id'      => Route::where('name', 'admin.candidate-followups.index')->first()->id,    // Route to customer candidate followup index
             'permission_id' => null,                                // Get permission from route
         ]);
 
