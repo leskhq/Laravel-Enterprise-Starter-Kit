fe25d947e7e772e598535d3d63e956308c0f9e95
aquino <aquinotgr00@gmail.com>
2016-04-28 13:53:36 +0700

    minor improvement
    - welcome page
    - indentation on sales create
    - small adjusting on master layout

--- resources/themes/default/layouts/master.blade.php
+++ resources/themes/default/layouts/master.blade.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html>
   <head>
     <meta charset="UTF-8">
--- resources/themes/default/views/admin/sales/create.blade.php
+++ resources/themes/default/views/admin/sales/create.blade.php
@@ -15,6 +15,7 @@
             <div class="box box-primary" style="float:none;margin:0 auto;">
                 <div class="box-header with-border">
                     <h3 class="box-title">{{ trans('admin/sales/general.page.create.section-title') }}</h3>
+
                     <div class="box-tools pull-right">
                         <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
                     </div>
