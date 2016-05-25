5ce0a95d75ae8fcc149effbdee89f5e429bcf6ef
Vick G <vg@wisercapital.com>
2016-05-25 11:54:29 -0700

    added footer formatting, styles, social media icons, links

--- resources/themes/wisercapital/partials/_body_footer.blade.php
+++ resources/themes/wisercapital/partials/_body_footer.blade.php
@@ -1,9 +1,70 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        {!! config('app.tag_line') !!}
-    </div>
-    <!-- Default to the left -->
-    {!! config('app.copyright_line') !!}
+	
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6">
+			<ul class="list-inline">
+				<li><a href="#">ABOUT WISER</a></li>
+				<li><a href="#">ENERGY INVESTMENTS</a></li>
+				<li><a href="#">HOST FACILITIES</a></li>
+				<li><a href="#">SYSTEM INTEGRATION</a></li>
+			</ul>
+		</div>
+		<div class="col-lg-6">
+			<div class="pull-right">
+				© WISERCAPITAL 2016. ALL RIGHTS RESERVED WORLDWIDE.
+			</div>
+		</div>
+		
+	</div>
+	
+	<div class="row">
+		<div class="col-lg-4">
+			<div class="row">
+				<div class="col-lg-1">
+					<a href="{!! config('app.facebook') !!}" class="socialicon facebook" target="_blank">Facebook</a>
+				</div>
+				<div class="col-lg-1">
+					<a href="{!! config('app.linkedin') !!}" class="socialicon linkedin" target="_blank">Linked In</a>
+				</div>
+				<div class="col-lg-1">
+					<a href="{!! config('app.twitter') !!}" class="socialicon twitter" target="_blank">Twitter</a>
+				</div>
+				<div class="col-lg-1">
+					<a href="{!! config('app.googleplus') !!}" class="socialicon googleplus" target="_blank">Google Plus</a>
+				</div>
+				<div class="col-lg-1">
+					<a href="{!! config('app.blog') !!}" class="socialicon blogicon">Blog</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-8">
+			<div class="pull-right">
+				<ul class="list-inline">
+					<li><a href="#">CONTACT</a> &nbsp;&nbsp;|</li>
+					<li><a href="#">TERMS & CONDITIONS</a> &nbsp; &nbsp;|</li>
+					<li><a href="#">PRIVACY POLICY</a></li>
+				</ul>
+			</div>
+		</div>
+		
+	</div>
+</div>		
+			
+
+<!-- 
+
+				
+    	
+			{!! config('app.copyright_line') !!}
+			
+-->
+		
+
+    
+    
+    
+		
+		
 </footer>
