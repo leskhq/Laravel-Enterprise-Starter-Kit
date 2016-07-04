3224a2745dcc95a45ee455ff1c989a50ba6d92ed
sroutier <sebastien.routier@international.gc.ca>
2016-07-04 16:58:24 -0400

    Add specific environment settings for sqlsrv database drivers.

--- config/database.php
+++ config/database.php
@@ -77,10 +77,11 @@ return [
 
         'sqlsrv' => [
             'driver'   => 'sqlsrv',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => env('DB_SQLSRV_HOST',     'localhost'),
+            'port'     => env('DB_SQLSRV_PORT',     '1433'),
+            'database' => env('DB_SQLSRV_DATABASE', 'l51esk'),
+            'username' => env('DB_SQLSRV_USERNAME', 'l51esk'),
+            'password' => env('DB_SQLSRV_PASSWORD', 'Password1'),
             'prefix'   => '',
         ],
 
