ad7973a3f4791ff7619addb9d20803c6545a1863
Sebastien Routier <sroutier@gmail.com>
2016-11-03 14:55:34 -0400

    Add other modal styles: primary, info, warning, success & danger.

--- resources/themes/default/partials/_body.blade.php
+++ resources/themes/default/partials/_body.blade.php
@@ -30,8 +30,48 @@ desired effect
     <!-- Optional bottom section for modals etc... -->
     @yield('body_bottom')
 
-    <!-- Body Bottom modal dialog-->
+    <!-- Body Bottom modal DEFAULT dialog-->
     <div class="modal fade" id="modal_dialog" tabindex="-1" role="dialog" aria-labelledby="modal_dialog_title" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            </div>
+        </div>
+    </div>
+
+    <!-- Body Bottom modal PRIMARY dialog-->
+    <div class="modal modal-primary fade" id="modal_dialog_primary" tabindex="-1" role="dialog" aria-labelledby="modal_dialog_title" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            </div>
+        </div>
+    </div>
+
+    <!-- Body Bottom modal INFO dialog-->
+    <div class="modal modal-info fade" id="modal_dialog_info" tabindex="-1" role="dialog" aria-labelledby="modal_dialog_title" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            </div>
+        </div>
+    </div>
+
+    <!-- Body Bottom modal WARNING dialog-->
+    <div class="modal modal-warning fade" id="modal_dialog_warning" tabindex="-1" role="dialog" aria-labelledby="modal_dialog_title" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            </div>
+        </div>
+    </div>
+
+    <!-- Body Bottom modal SUCCESS dialog-->
+    <div class="modal modal-success fade" id="modal_dialog_success" tabindex="-1" role="dialog" aria-labelledby="modal_dialog_title" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            </div>
+        </div>
+    </div>
+
+    <!-- Body Bottom modal DANGER dialog-->
+    <div class="modal modal-danger fade" id="modal_dialog_danger" tabindex="-1" role="dialog" aria-labelledby="modal_dialog_title" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
             </div>
