6fc976f4bb5a0198eced29f8322ff5df00524556
Sebastien Routier <sroutier@gmail.com>
2016-12-02 11:30:11 -0500

    Change Gravatar config to use secure conenction.

--- config/gravatar.php
+++ config/gravatar.php
@@ -13,7 +13,7 @@ return array(
         'fallback' => 'identicon',
 
         // would you like to return a https://... image
-        'secure' => false,
+        'secure' => true,
 
         // Gravatar allows users to self-rate their images so that they can indicate if an image
         // is appropriate for a certain audience. By default, only 'G' rated images are displayed
