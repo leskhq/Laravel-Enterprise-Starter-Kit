145cf5c28c20bc553a57ace4618cd575a3d49193
Sebastien Routier <sroutier@gmail.com>
2017-07-24 12:09:53 -0400

    Require package doctrine/dbal. Require package barryvdh/laravel-ide-helper. Fix index length for older mysql/mariadb.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Set index lengths for older MySQL / MariaDB.
+        // See: https://laravel.com/docs/5.4/migrations#indexes
+        Schema::defaultStringLength(191);
     }
 
     /**
@@ -23,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Register IDE-HELPER only on non-production environment.
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 }
