fa40601de235e954bb8208526ece7904696105a1
Sebastien Routier <sroutier@gmail.com>
2017-08-09 15:29:48 -0400

    Tweak the user table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('enabled')->default(false);
+            $table->string('auth_type')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
