0962898191509c8883a6ba50dba7845d0ba4b54b
Sebastien Routier <sroutier@gmail.com>
2017-08-22 16:46:27 -0400

    Adding bootstrap-tagsinput to render tags

---
+++ resources/themes/default/partials/_body_bottom_tags_js.blade.php
@@ -0,0 +1,3 @@
+<!-- Bootstrap Tags Input 0.8 -->
+<script src="{{ asset("/bootstrap-tagsinput/dist/bootstrap-tagsinput.js") }}"></script>
+
---
+++ resources/themes/default/partials/_head_extra_tags_css.blade.php
@@ -0,0 +1,2 @@
+<!-- Bootstrap Tags Input 0.8 -->
+<link href="{{ asset("/bootstrap-tagsinput/dist/bootstrap-tagsinput.css") }}" rel="stylesheet" type="text/css" />
