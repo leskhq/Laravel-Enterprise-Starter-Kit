d98b08f051c6353d203102ef7023f594720adc5c
MeticulousGriffon <noah.routier@gmail.com>
2017-10-09 14:56:58 -0400

    Compile resources and add fonts.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,10 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sourceMaps()
+    .version();
+
+// if (mix.inProduction()) {
+//     mix.version();
+// }
