111acde4da542969b51e5525fd30e3b958fccd06
Sebastien Routier <sroutier@gmail.com>
2017-10-20 14:48:48 -0400

    Make the Audit middleware aware of the global enable/disable setting.

--- app/Http/Middleware/Audit.php
+++ app/Http/Middleware/Audit.php
@@ -9,6 +9,7 @@ use App\Models\Audit as Auditor;
 use Illuminate\Http\Request;
 use Log;
 use Route;
+use Settings;
 
 class Audit
 {
@@ -23,49 +24,53 @@ class Audit
     {
         $userid  = null;
         $attJson = null;
-        $agent   = new Agent();
+        $audit_enabled = Settings::get('audit.enabled', false);
 
-        if ( Auth::check() ) {
-            $userid = Auth::user()->id;
-        }
+        if ($audit_enabled) {
+            $agent = new Agent();
 
-        // Get all attribute from the request.
-        $attributes = $request->all();
-        if (isset($attributes) && is_array($attributes)) {
-            // Remove from array attributes that we do not want to save.
-            unset($attributes['_method']);
-            unset($attributes['_token']);
-            unset($attributes['password']);
-            unset($attributes['password_confirmation']);
-
-            if ($attributes) {
-                $attJson = json_encode($attributes);
+            if (Auth::check()) {
+                $userid = Auth::user()->id;
             }
+
+            // Get all attribute from the request.
+            $attributes = $request->all();
+            if (isset($attributes) && is_array($attributes)) {
+                // Remove from array attributes that we do not want to save.
+                unset($attributes['_method']);
+                unset($attributes['_token']);
+                unset($attributes['password']);
+                unset($attributes['password_confirmation']);
+
+                if ($attributes) {
+                    $attJson = json_encode($attributes);
+                }
+            }
+
+            $routeName = Route::currentRouteName();
+            $routeAction = Route::getCurrentRoute()->getActionName();
+
+            Log::debug("Audit: Login request for action: " . $routeAction);
+
+            Auditor::create([
+                'user_id' => $userid,
+                'method' => $request->getMethod(),
+                'path' => $request->getPathInfo(),
+                'route_name' => $routeName,
+                'route_action' => $routeAction,
+                'query' => $request->getQueryString(),
+                'data' => $attJson,
+                'userAgent' => $agent->getUserAgent(),
+                'ip' => \Request::ip(),
+                'device' => $agent->device(),
+                'platform' => $agent->platform(),
+                'browser' => $agent->browser(),
+                'isDesktop' => $agent->isDesktop(),
+                'isMobile' => $agent->isMobile(),
+                'isPhone' => $agent->isPhone(),
+                'isTablet' => $agent->isTablet()
+            ]);
         }
-
-        $routeName = Route::currentRouteName();
-        $routeAction = Route::getCurrentRoute()->getActionName();
-
-        Log::debug("Audit: Login request for action: ". $routeAction);
-
-        Auditor::create([
-            'user_id'      => $userid,
-            'method'       => $request->getMethod(),
-            'path'         => $request->getPathInfo(),
-            'route_name'   => $routeName,
-            'route_action' => $routeAction,
-            'query'        => $request->getQueryString(),
-            'data'         => $attJson,
-            'userAgent'    => $agent->getUserAgent(),
-            'ip'           => \Request::ip(),
-            'device'       => $agent->device(),
-            'platform'     => $agent->platform(),
-            'browser'      => $agent->browser(),
-            'isDesktop'    => $agent->isDesktop(),
-            'isMobile'     => $agent->isMobile(),
-            'isPhone'      => $agent->isPhone(),
-            'isTablet'     => $agent->isTablet()
-        ]);
 
         return $next($request);
     }
