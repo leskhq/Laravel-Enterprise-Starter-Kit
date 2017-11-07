5150657bdbb422dce400c30371e547e3fe0f8337
Sebastien Routier <sroutier@gmail.com>
2017-11-07 13:00:14 -0500

    Add util function to Str class.

--- app/Libraries/Str.php
+++ app/Libraries/Str.php
@@ -160,4 +160,18 @@ class Str extends BaseStr
         }
     }
 
+    public static function startsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle);
+    }
+
+    public static function endsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+
+        return $length === 0 ||
+            (substr($haystack, -$length) === $needle);
+    }
+
 }
