44288c6b6655e8efb1aaa59386c7aaf6f6947082
Sebastien Routier <sroutier@gmail.com>
2017-11-24 13:11:29 -0500

    Update module config file.

--- config/modules.php
+++ config/modules.php
@@ -17,7 +17,33 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Modules Default State
+    | Path to public assets
+    |--------------------------------------------------------------------------
+    |
+    | Define the path where the public assets of each module will be deployed to
+    | to be available under the public directory.
+    |
+    */
+
+    'path_public_assets' => public_path('vendor/modules'), // Set to public/vendor/modules
+
+    /*
+    |--------------------------------------------------------------------------
+    | Modules Default Initialized State
+    |--------------------------------------------------------------------------
+    |
+    | When a previously unknown module is added, if it doesn't have an
+    | 'initialized' state set then this is the value which it will
+    | default to. If this is not provided then the module will
+    | default to being 'initialized'.
+    |
+    */
+
+    'initialized' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Modules Default Enabled State
     |--------------------------------------------------------------------------
     |
     | When a previously unknown module is added, if it doesn't have an 'enabled'
@@ -50,11 +76,24 @@ return [
     | This driver manages the retrieval and management of module properties.
     | Setting this to custom allows you to specify your own driver instance.
     |
-    | Supported: "local"
+    | Supported: "local", "redis", "custom"
     |
     */
 
     'driver' => 'local',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Module Driver
+    |--------------------------------------------------------------------------
+    |
+    | This option allows one to define a custom module driver implementation.
+    | This is useful in cases where you may need to support and store module
+    | properties somewhere not supported by default.
+    |
+    */
+
+    'custom_driver' => 'App\Repositories\Modules\CustomRepository',
 
     /*
     |--------------------------------------------------------------------------
